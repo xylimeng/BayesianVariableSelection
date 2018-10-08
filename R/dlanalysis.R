0d737bb845bd5909f118aee5a32e8229b4651751
scarlettshijiazhang <43660111+scarlettshijiazhang@users.noreply.github.com>
2018-10-08 21:27:50 +0800

    Delete dlanalysis.R

--- R/dlanalysis.R
+++
@@ -1,9 +0,0 @@
-
-dlanalysis<-function(dlresult,alpha=0.05){
-  betamean=apply(dlresult,2,mean)
-  betamedian=apply(dlresult,2,median)
-  min=apply(dlresult,2,quantile,prob=alpha/2)
-  max=apply(dlresult,2,quantile,prob=1-alpha/2)
-  result=list("betamean"=betamean,,"betamedian"=betamedian"LeftCI"=min,"RightCI"=max)
-  return(result)
-}
