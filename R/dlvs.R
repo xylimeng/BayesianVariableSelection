c1de847fa376c0ac199e6eb5f98f249854b9e3c4
scarlettshijiazhang <43660111+scarlettshijiazhang@users.noreply.github.com>
2018-10-09 17:23:33 +0800

    Update dlvs.R

--- R/dlvs.R
+++ R/dlvs.R
@@ -1,4 +1,6 @@
 dlvs<-function(dlresult){
+  #use penalized credible region to do variable selection
+  #matrix computation to make the solutions be accomplished by LASSO
   p=ncol(dlresult)
   betamean<-apply(dlresult,2,mean)
   betacov<-cov(dlresult)
@@ -7,8 +9,10 @@ dlvs<-function(dlresult){
   covinv<-MASS::ginv(cov1)
   xstar=t(as.numeric(D)*covinv)
   ystar=covinv%*%betamean
+  #scale
   xstar<-scale(xstar, scale=F)
   ystar<-ystar-mean(ystar)
+  #solve LASSO problem by glmnet package
   model<-glmnet::glmnet(xstar,ystar,standardize=FALSE,alpha=1,family="gaussian")
   lam=glmnet::cv.glmnet(xstar,ystar,standardize=FALSE,alpha=1,family="gaussian")$lambda.1se
   betastar=coef(model,s=lam)[2:(p+1)]
